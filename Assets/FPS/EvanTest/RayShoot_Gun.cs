2c77a3947d47d2024a48ce5dc771fc247d9bf644
Evan Huang <huanh8@users.noreply.github.com>
2022-08-01 13:47:43 +1200

    add hit effect

--- Assets/FPS/EvanTest/RayShoot_Gun.cs
+++ Assets/FPS/EvanTest/RayShoot_Gun.cs
@@ -8,9 +8,12 @@ namespace Unity.FPS.Gameplay
 {
     public class RayShoot_Gun : MonoBehaviour
     {
-        public Transform fpsCam;
+        public GameObject HitEffect;
+        private ParticleSystem[] Hit;
+        public Camera fpsCam;
         public LineRenderer lineRenderer;
-        public float range = 100f;
+        public float range = 1000f;
+        public float HitOffset = 0;
 
         // Update is called once per frame
         private PlayerInputHandler InputHandler;
@@ -18,7 +21,9 @@ namespace Unity.FPS.Gameplay
         void Start()
         {
             GameObject player = GameObject.Find("Player");
+            fpsCam = player.transform.GetChild(0).GetChild(0).GetComponent<Camera>();
             InputHandler = player.GetComponent<PlayerInputHandler>();
+            Hit = HitEffect.GetComponentsInChildren<ParticleSystem>();
         }
         void Update()
         {
@@ -30,9 +35,11 @@ namespace Unity.FPS.Gameplay
             if (InputHandler.GetFireInputReleased())
             {
                 lineRenderer.enabled = false;
+                HitEffect.active = false;
             }
         }
 
+        [System.Obsolete]
         void ShootRay()
         {
             RaycastHit hit;
@@ -40,13 +47,25 @@ namespace Unity.FPS.Gameplay
             if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
             {
                 Debug.Log(hit.transform.name);
-                lineRenderer.SetPosition(0, fpsCam.transform.position);
+
+                lineRenderer.SetPosition(0, lineRenderer.transform.position);
                 lineRenderer.SetPosition(1, hit.point);
+                HitEffect.active = true;
+                HitEffect.transform.position = hit.point + hit.normal * HitOffset;
+                HitEffect.transform.rotation = Quaternion.identity;
+                //get the ditance between the hit point and the player
+                float distance = Vector3.Distance(hit.point, transform.position);
+                Debug.Log(distance);
+                //HitEffect.transform.position = lineRenderer.GetPosition(1) + hit.normal * 0.1f;
             }
             else
-            {
-                lineRenderer.SetPosition(0, fpsCam.transform.position);
-                lineRenderer.SetPosition(1, fpsCam.transform.position + fpsCam.transform.forward * 1000);
+            { //End laser position if doesn't collide with object
+                var EndPos = fpsCam.transform.position + fpsCam.transform.forward * 10000;
+
+                lineRenderer.SetPosition(0, lineRenderer.transform.position);
+                lineRenderer.SetPosition(1, EndPos);
+                HitEffect.transform.position = EndPos;
+                HitEffect.active = false;
             }
         }
 
