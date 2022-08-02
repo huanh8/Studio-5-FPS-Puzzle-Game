e7bfc28d8e26bb4e3adf1d5dccd9cc856ab4d050
PANZ5 <panz5@student.op.ac.nz>
2022-08-02 09:16:09 +0800

    Set up basic Scriptable Object based script

---
+++ Assets/FPS/Scripts/ScriptableObject/trigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Trigger", menuName = "Trigger")]
+public class trigger : ScriptableObject
+{
+    public string triggerName;
+
+    // testing purpose
+    public string hitInfo;
+
+    public int angle;
+    public int chargeTime;
+    public bool isTriggerOn;
+}
