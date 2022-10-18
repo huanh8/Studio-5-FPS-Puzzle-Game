a606124c46da51418a3505a1c62d4cc596be5a31
Evan Huang <huanh8@users.noreply.github.com>
2022-10-19 12:50:47 +1300

    add stroy subtitle,audio and triggers

--- Assets/MenuManager/StartMenu.cs
+++ Assets/MenuManager/StartMenu.cs
@@ -7,15 +7,20 @@ namespace Unity.FPS.UI
     public class StartMenu : MonoBehaviour
     {
         public GameObject playerObject;
+        public GameObject storyManager;
         void OnDisable()
         {
-            if(playerObject != null)
+            if (playerObject != null)
                 playerObject.SetActive(true);
+            if (storyManager != null)
+                storyManager.SetActive(true);
         }
         void OnEnable()
         {
-            if(playerObject != null)
+            if (playerObject != null)
                 playerObject.SetActive(false);
+            if (storyManager != null)
+                storyManager.SetActive(false);
         }
     }
 }
